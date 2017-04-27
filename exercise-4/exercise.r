228cf13060deec620d1ba0344fb102d68003b212
anushreeshuk <anushreeshuk@gmail.com>
2017-04-26 23:39:59 -0700

    Section work

--- exercise-4/exercise.r
+++ exercise-4/exercise.r
@@ -1,3 +1,4 @@
+
 # Install and load the jsonlite package
 library(jsonlite)
 library(httr)
@@ -5,25 +6,33 @@ library(httr)
 # Make a variable base.url that has the same base url from the omdb documentation.
 # (Hint: visit https://www.omdbapi.com/ to find the base url)
 
+base.url <- 'http://www.omdbapi.com/?'
 
 # Make a variable called movie that has the name of your favorite movie
 
+movie <- 'Inception'
 
 # Make a variable called "query.parameters" that holds a list of the parameters
 # to pass to the API. View the OMDb documentation to see which parameters
 # are available.
 
+query.parameters <- list(t = movie, r= 'json')
+
 
 # Make a variable called request that is a string of a request URL made up of the base URL
 # and the parameters string
 
+request <- GET(base.url, query = query.parameters)
+body <- content(request, "text")
 
 # Use fromJSON to retrieve JSON data from the omdb api using your request.
 # Store the result in a variable called movie.data
 
+movie.data <- fromJSON(body())
 
 # Make movie_data into a data frame using as.data.frame
 
+movie.df <- as.data.frame(movie.data)
 
 # Write a function called Director that accepts a data frame of movie info and returns
 # A vector of strings that states a movie and the director of said movie.
@@ -42,10 +51,9 @@ library(httr)
 
 # Write a function to make your API request and process the data
 
-  # Make a variable called request that is a string of a request URL made up of the base URL
-  # and the parameters string
+# Make a variable called request that is a string of a request URL made up of the base URL
+# and the parameters string
 
 # For every entry in the vector api.request, APPLY the function fromJSON to make a list of lists
 # one entry for each request and assign this to a variable called data. 
 # (Hint: ?lapply. It's similar a 'for' loop but better!)
-
